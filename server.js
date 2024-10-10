c55af97d1eb37691d1802abdc36f8c1389d9c89c
amalieasborg <amalieasborg@gmail.com>
2024-10-10 12:48:51 +0200

    Tilføjede middleware til statiske filer

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.use(express.json())
+app.use(express.json());
 
 app.use((req,res,next)=>{
-    console.log(`${req.method} request for ${req.url}`)
-    next()
-})
+    console.log(`${req.method} request for ${req.url}`);
+    next();
+});
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname+'/public/views/homepage.html')
-})
+    res.sendFile(__dirname+'/public/views/homepage.html');
+});
+
+// Middleware til at tjene statiske filer fra 'public'-mappen
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}/`)
-})
+    console.log(`Server started at http://localhost:${port}/`);
+});
