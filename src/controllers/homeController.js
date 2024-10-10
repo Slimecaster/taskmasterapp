2506af7881a84e847486cae7a5cc0fb799364c73
Slimecaster <nd070503@gmail.com>
2024-10-10 17:09:09 +0200

    hi

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,6 @@
 const path = require("path");
 
 exports.getHomePage = (req, res) => {
-    res.sendFile(path.join('../views/homepage.html'));
+    res.sendFile(path.join('C:/Users/nd070/WebstormProjects/taskmasterapp/public/views/homepage.html'));
 }
 
